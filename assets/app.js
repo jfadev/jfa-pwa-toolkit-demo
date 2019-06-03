1a118413da28231dc1434b43343c495279e6cb2d
jfadev <jfadev@gmail.com>
2019-06-02 22:53:18 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
+// Register Service Worker
 PWA.ServiceWorker.register();
 
+// Init Materialize
 M.AutoInit();
-
-
 
 // Displays a pop-up requesting permission to allow Notifications
 document
@@ -13,7 +13,6 @@ document
         });
     })
 ;
-
 
 // Show Notification sent by the browser
 document
@@ -33,10 +32,6 @@ document
         });
     })
 ;
-
-
-
-
 
 // Button to subscribe to Push Notifications
 document
@@ -86,7 +81,6 @@ document
     })
 ;
 
-
 // Clear the browser app cache
 document
     .getElementById('btn-clear-cache')
@@ -95,106 +89,3 @@ document
         M.toast({html: "Clear Cache! Look in Dev tools > Application!"});
     })
 ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Check the Notifications permissions
-// // Default status
-// if (PWA.Notification.isDefault()) {
-//     console.log('Permission for Notifications are with default status.');
-// } else {
-//     console.log('Permission for Notifications are not with default status.');
-// }
-//
-// // Granted status
-// if (PWA.Notification.isGranted()) {
-//     console.log('Permission for Notifications was granted!');
-// } else {
-//     console.log('Permission for Notifications was not granted!');
-// }
-//
-// // Blocked status
-// if (PWA.Notification.isBlocked()) {
-//     console.log('Permission for Notifications was blocked.');
-// } else {
-//     console.log('Permission for Notifications was not blocked.');
-// }
-//
-// // Denied status
-// if (PWA.Notification.isDenied()) {
-//     console.log('Permission for Notifications was denied.');
-// } else {
-//     console.log('Permission for Notifications was not denied.');
-// }
-//
-// // Displays a pop-up requesting permission to allow Notifications
-// PWA.Notification.requestPermission((status) => {
-//     console.log('Notification permission status:', status);
-// });
-//
-// // Get the Notifications permission status
-// var permission = PWA.Notification.getPermission();
-//
-// // Show Notification sent by the browser
-// const options = {
-//     body: 'Extra content to display within the notification',
-//     icon: '../images/touch/chrome-touch-icon-192x192.png'
-// };
-//
-// PWA.Notification.show('Notification Title', options, sent => {
-//     if (sent) {
-//     	console.log('The Notification has been sent');
-//     }
-// });
-//
-// // Register the main service worker
-// PWA.ServiceWorker.register();
-//
-// // Get the Registration object of the service worker
-// PWA.ServiceWorker.getRegistration((registration) => {
-//     if (registration) {
-//     	console.log(registration);
-//     } else {
-//        console.log('The Service Worker is not registered!');
-//     }
-// });
-//
-// // Check if the browser support Service Workers
-// if (PWA.Navigator.isSupportedServiceWorker()) {
-//     console.log('This browser support Service Workers!');
-// } else {
-//     console.log('This browser does not support Service Workers.');
-// }
-//
-// // Check if the browser support Notifications
-// if (PWA.Navigator.isSupportedNotification()) {
-//     console.log('This browser support Notifications!');
-// } else {
-//     console.log('This browser does not support Notifications.');
-// }
-//
-// // Check if the browser is offline
-// if (PWA.Navigator.isOffline()) {
-//     console.log('No Internet connection!');
-// } else {
-//     console.log('You have an Internet connection!');
-// }
-//
-// // Clear the browser app cache
-// PWA.Navigator.clearCache();
